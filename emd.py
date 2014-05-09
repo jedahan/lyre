b30b4a93bb10537fbfd69033b788d0b75fbb1203
Jonathan Dahan <jonathan@jedahan.com>
2014-05-09 12:05:23 -0400

    move needles to subdirectory

--- emd.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import cv2
-img = cv2.imread('compare.jpg')
-
-def recalculate_histograms
-  for channel in [0,1,2]
-    for image in images
-      histograms[image.filename] = cv2.calcHist(image,channel,None,[256],[0,256])
-
